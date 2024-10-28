288b3106cfc1761aa6bff6d799d7bbaf4f682e80
greenstorm5417 <sdussinger1007@gmail.com>
2024-10-28 18:35:40 -0400

    v1.1 to fix read me on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="path_friendly_filename_generator",
-    version="0.1.0",
+    version="0.1.1",
     author="Sammy",
     author_email="dussinsa01@esj.org",
     description="A package to generate path-friendly filenames across different operating systems.",
